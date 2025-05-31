43700954692569931b23fb6926af59aeaeedebe4
SahilAggarwal9155 <sahilaggarwal894@gmail.com>
2025-05-31 15:39:20 +0530

    Implement login functionality with useLogin hook, enhance LoginForm component, and add FormRowVertical for improved layout

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin(){
+  const navigate = useNavigate();
+
+  const {mutate : login, isPending} = useMutation({
+    mutationFn: ({email, password}) => loginApi({email, password}),
+    onSuccess: (user)=>{
+      console.log(user);
+      navigate('/dashboard');
+    },  
+    onError: (err)=>{
+       console.log("Error", err);
+       toast.error('Provided Email and Password are incorrect');
+    }
+
+  })
+
+  return {login, isPending};
+}
