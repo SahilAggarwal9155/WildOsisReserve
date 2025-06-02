2251a2a0d79b8ba8beba54eba8cc3b9e7b462be9
SahilAggarwal9155 <sahilaggarwal894@gmail.com>
2025-06-02 13:01:37 +0530

    Add user avatar functionality, enhance user data update forms, and improve dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,15 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today activities</div>
+      <div>Chart stay duration</div>
+      <div>chart sales</div>
+    </StyledDashboardLayout>
+  )
+}
